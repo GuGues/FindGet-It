25aaf5749770d3d36b3b5185242cd9c295931079
GuGues <akdrn113@naver.com>
2024-12-26 12:29:08 +0900

    lim su jer jang

--- get/src/main/java/com/get/mypage/MypageController.java
+++ get/src/main/java/com/get/mypage/MypageController.java
@@ -1,14 +1,12 @@
 package com.get.mypage;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -17,11 +15,11 @@ public class MypageController {
     @Autowired
     private MypageMapper mypageMapper;
 
-   @RequestMapping("/Mypage")
-   public String() {
-	   retrun "hi";
-   }
-    
+    @GetMapping("/api/example")
+    public ResponseEntity<String> example() {
+        return ResponseEntity.ok("Success");
+    }
+   
     
    
     
