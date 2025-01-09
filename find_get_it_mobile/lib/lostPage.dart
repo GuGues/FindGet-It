e6053df136db062ea1175a5b6a7b08837eab2ce3
LGHdeveloper95 <gh950227@gmail.com>
2025-01-09 14:49:43 +0900

    웹 절차 버튼 앱 뷰폼

--- find_get_it_mobile/lib/lostPage.dart
+++ find_get_it_mobile/lib/lostPage.dart
@@ -160,7 +160,6 @@ class LostPage extends StatefulWidget {
 
 class _LostPageState extends State<LostPage> {
   int page = 1;
-  late int pageCnt;
   TextEditingController startDateController = TextEditingController();
   TextEditingController endDateController = TextEditingController();
   TextEditingController titleController = TextEditingController();
@@ -168,33 +167,16 @@ class _LostPageState extends State<LostPage> {
   String selectedCategory = '물품 카테고리';
   String selectedLocation = '장소';
   String selectedColor = '색상';
-  String selectedStartDate = '시작일';
-  String selectedEndDate = '종료일';
 
-  searchItems(int page) async {
-    var params = '';
-    if(lost_title!=null && lost_title.length!=0){
-      params += '&lost_title=$lost_title';
-    }if(item_code!=null && item_code!=0){
-      params += '&item_code=$item_code';
-    }if(location_code!=null && location_code!=0){
-      params += '&location_code=$location_code';
-    }if(color_code!=null && color_code!=0){
-      params += '&color_code=$color_code';
-    }if(start_date!=null && start_date.length!=0){
-      params += '&start_date=$start_date';
-    }if(end_date!=null && end_date.length!=0){
-      params += '&end_date=$end_date';
-    }
+  searchItems(int page, Map<String, dynamic> map) async {
+    print(map);
     final response = await http.get(
-        Uri.parse('${appConfig.url}/app/getLostSearch?page=$page$params'));
+        Uri.parse('${appConfig.url}/app/getLostSearch?page=$page&lost_title=$lost_title'));
     if (response.statusCode == 200) {
-      Map<String, dynamic> data = json.decode(utf8.decode(response.bodyBytes));
-      List<dynamic> searchLostList = data["searchLost"];
+      List<dynamic> data = json.decode(utf8.decode(response.bodyBytes));
       print(data);
       setState(() {
-        pageCnt = data["pageCnt"];
-        lostList = searchLostList.map((json) => Lost.fromJson(json)).toList();
+        lostList = data.map((json) => Lost.fromJson(json)).toList();
       });
     } else {
       throw Exception('Failed to load items');
@@ -206,11 +188,9 @@ class _LostPageState extends State<LostPage> {
     await http.get(Uri.parse('${appConfig.url}/app/lostList?page=$page'));
 
     if (response.statusCode == 200) {
-      Map<String, dynamic> data = json.decode(utf8.decode(response.bodyBytes));
-      List<dynamic> getLostList = data["lostList"];
+      List<dynamic> data = json.decode(utf8.decode(response.bodyBytes));
       setState(() {
-        pageCnt = data["pageCnt"];
-        lostList= getLostList.map((json) => Lost.fromJson(json)).toList();
+      lostList= data.map((json) => Lost.fromJson(json)).toList();
       });
     } else {
       throw Exception('Failed to load items');
@@ -283,9 +263,6 @@ class _LostPageState extends State<LostPage> {
                 case 1:
                   Navigator.pushNamed(context, '/lost');
                   break;
-                  case 2:
-                  Navigator.pushNamed(context, '/found');
-                  break;
                 default:
                   print("Menu item $item selected");
               }
@@ -309,9 +286,10 @@ class _LostPageState extends State<LostPage> {
               children: [
                 Container(
                     width: double.infinity,
-                    padding: EdgeInsets.all(30.0),
+                    padding: const EdgeInsets.all(6.0),
+                    // padding 줄이기
                     margin: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 15.0),
+                        horizontal: 16.0, vertical: 4.0),
                     // margin 줄이기
                     decoration: BoxDecoration(
                       color: Colors.white,
@@ -440,11 +418,9 @@ class _LostPageState extends State<LostPage> {
                               },
                               child: Text(selectedCategory),
                               style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.black54,
-                                backgroundColor: Colors.orangeAccent,
                                 padding: EdgeInsets.symmetric(
                                     vertical: 12, horizontal: 20),
-                                textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold,),
+                                textStyle: TextStyle(fontSize: 14),
                               ),
                             ),
                           ],
@@ -551,11 +527,9 @@ class _LostPageState extends State<LostPage> {
                               },
                               child: Text(selectedLocation),
                               style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.black54,
-                                backgroundColor: Colors.orangeAccent,
                                 padding: EdgeInsets.symmetric(
                                     vertical: 12, horizontal: 20),
-                                textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                                textStyle: TextStyle(fontSize: 14),
                               ),
                             ),
                           ],
@@ -581,6 +555,14 @@ class _LostPageState extends State<LostPage> {
                                 final colorList = jsonResponse
                                     .map((item) => color.fromJson(item))
                                     .toList();
+
+                                //var lost_title = '';
+                                //var item_code = '';
+                                //var location_code = '';
+                                //var start_date = '';
+                                //var end_date = '';
+                                //var color_code = '';
+
                                 // 모달 바텀 시트 호출
                                 showModalBottomSheet(
                                   context: context,
@@ -613,11 +595,9 @@ class _LostPageState extends State<LostPage> {
                               },
                               child: Text(selectedColor),
                               style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.black54,
-                                backgroundColor: Colors.orangeAccent,
                                 padding: EdgeInsets.symmetric(
                                     vertical: 12, horizontal: 20),
-                                textStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.
+                                textStyle: TextStyle(fontSize: 14),
                               ),
                             ),
                           ],
@@ -638,7 +618,7 @@ class _LostPageState extends State<LostPage> {
                                 controller: startDateController,
                                 readOnly: true, // 텍스트 입력 방지
                                 decoration: InputDecoration(
-                                  hintText: selectedStartDate,
+                                  hintText: '시작일',
                                   border: OutlineInputBorder(),
                                   suffixIcon: Icon(Icons.calendar_today),
                                 ),
@@ -656,7 +636,7 @@ class _LostPageState extends State<LostPage> {
                                 controller: endDateController,
                                 readOnly: true, // 텍스트 입력 방지
                                 decoration: InputDecoration(
-                                  hintText: selectedEndDate,
+                                  hintText: '종료일',
                                   border: OutlineInputBorder(),
                                   suffixIcon: Icon(Icons.calendar_today),
                                 ),
@@ -671,55 +651,33 @@ class _LostPageState extends State<LostPage> {
 
                         // 검색 버튼
                         Center(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              ElevatedButton.icon(
-                                onPressed: () async {
-                                  setState(() {
-                                    lost_title = '';
-                                    item_code = 0;
-                                    location_code = 0;
-                                    start_date = '';
-                                    end_date = '';
-                                    startDateController.clear();
-                                    endDateController.clear();
-                                    color_code = 0;
-                                    selectedCategory = '물품 카테고리';
-                                    selectedLocation = '장소';
-                                    selectedColor = '색상';
-                                    selectedStartDate = '시작일';
-                                    selectedEndDate = '종료일';
-                                  });
-                                },
-                                icon: Icon(Icons.refresh),
-                                label: Text('초기화'),
-                                style: ElevatedButton.styleFrom(
-                                  foregroundColor: Colors.black54,
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 15, vertical: 10),
-                                  backgroundColor: Colors.grey,
-                                  textStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w800,),
-                                ),
-                              ),
-                              ElevatedButton.icon(
-                                onPressed: () async {
-                                  lost_title = titleController.text;
-                                  page=1;
-                                  searchItems(page);
-                                },
-                                icon: Icon(Icons.search),
-                                label: Text('검색'),
-                                style: ElevatedButton.styleFrom(
-                                  foregroundColor: Colors.white,
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 20, vertical: 12),
-                                  backgroundColor: Colors.orange,
-                                  textStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w800,),
-                                ),
-                              ),
-                            ],
-                          )
+                          child: ElevatedButton.icon(
+                            onPressed: () async {
+                              lost_title = titleController.text;
+
+                              Map<String, dynamic> map = {
+                                'lost_title': lost_title,
+                                'item_code': item_code,
+                                'location_code': location_code,
+                                'color_code': color_code,
+                                'start_date': start_date,
+                                'end_date': end_date,
+                              };
+                              print(map);
+                              searchItems(page, map);
+                              print("+++++++++++++++++++++++++++++");
+                              print(lostList[0].lostTitle);
+                              print("+++++++++++++++++++++++++++++");
+                            },
+                            icon: Icon(Icons.search),
+                            label: Text('검색'),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 12),
+                              backgroundColor: Colors.orange,
+                              textStyle: TextStyle(color: Colors.black),
+                            ),
+                          ),
                         ),
                       ],
                     )),
@@ -762,23 +720,17 @@ class _LostPageState extends State<LostPage> {
                         setState(() {
                           page--;
                         });
-                        searchItems(page);
                       }
                     },
                     child: Icon(Icons.arrow_back),
                   ),
                   SizedBox(width: 16),
-                  Text("$page"),
-                  SizedBox(width: 16),
                   ElevatedButton(
-                    onPressed: (page < pageCnt)
-                        ? () {
+                    onPressed: () {
                       setState(() {
                         page++;
                       });
-                      searchItems(page);
-                    }
-                        : null,  // page가 pageCnt와 같을 경우 버튼을 비활성화
+                    },
                     child: Icon(Icons.arrow_forward),
                   ),
                 ],
@@ -789,3 +741,83 @@ class _LostPageState extends State<LostPage> {
       ),
     );
   }
+
+  Widget buildSearchRow(String label, String value, BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(width: 16),
+        Expanded(
+          child: TextField(
+            style: TextStyle(fontSize: 14),
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+              border: OutlineInputBorder(),
+              hintText: value,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildSearchRowWithColor(String label, String value, Color color) {
+    return Row(
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(width: 16),
+        Container(
+          width: 20,
+          height: 20,
+          color: color,
+        ),
+        SizedBox(width: 8),
+        Text(value, style: TextStyle(fontSize: 14)),
+      ],
+    );
+  }
+
+  Widget buildTableHeader() {
+    return Row(
+      children: [
+        Expanded(
+            flex: 2,
+            child: Text('물품명', style: TextStyle(fontWeight: FontWeight.bold))),
+        Expanded(
+            flex: 1,
+            child: Text('게시일', style: TextStyle(fontWeight: FontWeight.bold))),
+        Expanded(
+            flex: 1,
+            child: Text('닉네임', style: TextStyle(fontWeight: FontWeight.bold))),
+      ],
+    );
+  }
+
+  Widget buildTableRow(
+      String itemName, String date, String nickname, String location) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(flex: 2, child: Text(itemName)),
+            Expanded(flex: 1, child: Text(date)),
+            Expanded(flex: 1, child: Text(nickname)),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text('분실 날짜: $date, $location',
+              style: TextStyle(color: Colors.grey, fontSize: 12)),
+        ),
+        Divider(),
+      ],
+    );
+  }
+}
